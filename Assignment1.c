937d3cc3c8d123ec58402c76bf85b8c5f7b2888e
bmarasi <danindum@outlook.com>
2020-10-03 00:07:25 -0400

    Finished 12 hour case for task 1

--- Assignment1.c
+++ Assignment1.c
@@ -8,8 +8,8 @@ Task 1: Get input from user of either 24 hour format or 12 hour format. Then dis
 ************************/
  
  int main(){
-    int hours, min, format;
-    //char[2] meridian;
+    int hour, min, format;
+    char meridian[2];
 
     //task 1
     //prompt for format until valid entry
@@ -18,7 +18,29 @@ Task 1: Get input from user of either 24 hour format or 12 hour format. Then dis
 
     //prompt for time based on format choice
     if(format == 1){
-         printf("Enter time in 12-hour format\n");
+        printf("Enter time in 12-hour format\n\n");
+        printf("Enter a value between 0 and 12 for hour: ");
+        scanf("%d", &hour);
+        printf("Enter a value between 0 and 60 for minutes: ");
+        scanf("%d", &min);
+        printf("Enter a for am or p for pm: ");
+        scanf("%s", meridian);
+        //determine output merdian based on input
+        if(meridian[0] == 'a'){
+            strcpy(meridian, "am");
+        }
+        else if(meridian[0] == 'p'){
+            strcpy(meridian, "pm");
+        }
+        printf("\n\n---------------------------\n");
+        printf("You entered %d%d:%d%d $s\n", (hour/10), (hour%10), (min/10), (min%10), meridian);
+        if(meridian[0] == 'a'){
+            printf("In 24 hour format, - you entered %d%d:%d%d\n", (hour/10), (hour%10), (min/10), (min%10));
+        }
+        else if(meridian[0] == 'p'){
+            printf("In 24 hour format, - you entered %d%d:%d%d\n", ((hour/10)+12), (hour%10), (min/10), (min%10));
+        }
+        printf("---------------------------\n");
     }
     else if(format == 2){
         printf("Enter time in 24-hour format\n");
